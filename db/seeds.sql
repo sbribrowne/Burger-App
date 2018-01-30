13d04178bfd515064dadb6b61798a2fec3c24a7b
sbribrowne <sbribrowne@gmail.com>
2018-01-29 19:16:45 -0600

    insert sql data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO cats (burger_name, devour) VALUES ('Pizza Burger', true);
+INSERT INTO cats (burger_name, devour) VALUES ('Blue Burger', false);
+INSERT INTO cats (burger_name, devour) VALUES ('Pickle Burger', true);
+INSERT INTO cats (burger_name, devour) VALUES ('American Burger', true);
+INSERT INTO cats (burger_name, devour) VALUES ('Cheese Burger', false);
