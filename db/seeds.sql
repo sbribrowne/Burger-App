02bf7a3e9a5d31e523758291658c8c7ebf20c669
sbribrowne <sbribrowne@gmail.com>
2018-01-29 20:16:29 -0600

    orm stuff added

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO cats (burger_name, devour) VALUES ('Pizza Burger', true);
-INSERT INTO cats (burger_name, devour) VALUES ('Blue Burger', false);
-INSERT INTO cats (burger_name, devour) VALUES ('Pickle Burger', true);
-INSERT INTO cats (burger_name, devour) VALUES ('American Burger', true);
-INSERT INTO cats (burger_name, devour) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pickle Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('American Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
