b9d033dc8873e50aa67a4c7f8b84a08ba33f03f7
sbribrowne <sbribrowne@gmail.com>
2018-01-30 20:46:09 -0600

    commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -20,7 +20,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+app.use("/", routes);
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
